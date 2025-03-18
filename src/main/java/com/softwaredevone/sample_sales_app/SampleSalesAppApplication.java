bfd8ac0eb1c80d0e38404ced069c33ededf2e753
Nitcelis Bravo <nitcelis.bravo@splgroup.com>
2025-03-17 20:42:08 -0400

    Add ENV variables to hide DB information in application.properties file

--- src/main/java/com/softwaredevone/sample_sales_app/SampleSalesAppApplication.java
+++ src/main/java/com/softwaredevone/sample_sales_app/SampleSalesAppApplication.java
@@ -1,12 +1,22 @@
 package com.softwaredevone.sample_sales_app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleSalesAppApplication {
 	public static void main(String[] args) {
+		// Load .env variables
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_HOST_AND_PORT", dotenv.get("DB_HOST_AND_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_USE_SSL", dotenv.get("DB_USE_SSL"));
+		System.setProperty("DB_SERVER_TIMEZONE", dotenv.get("DB_SERVER_TIMEZONE"));
+
+
 		SpringApplication.run(SampleSalesAppApplication.class, args);
 	}
-
 }
